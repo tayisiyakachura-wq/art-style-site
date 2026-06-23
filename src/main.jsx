8732170e0f07839d0b71290a7201fe4b52b096ef
tayisiyakachura-wq <tayisiyakachura@gmail.com>
2026-06-23 20:37:48 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './beauty-studio.tsx'
+import App from './beauty-studio.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
